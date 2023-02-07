bed790b8e18113aa3f82c19ae4f4b65c18cab3da
Pololokka <vitorbulbovas1@hotmail.com>
2023-02-07 13:13:27 -0300

    começando interatividade

---
+++ app/pegaInfo.js
@@ -0,0 +1,30 @@
+const nomeInput = document.querySelector("[data-nome]");
+const generoInput = document.querySelector("[data-genero]");
+const classeInput = document.querySelectorAll("[data-classe]");
+
+
+function pegaNome() {    
+    nomeInput.addEventListener('blur', () => {
+        nomeSelec = document.querySelector("[data-nome]").value;
+        nomeSelec == "" ? console.log("Inizad") : console.log(nomeSelec);
+    })
+}
+
+function pegaGenero() {
+    generoInput.addEventListener('blur', () => {
+        generoSelec = document.querySelector("[data-genero]").value;
+        console.log(generoSelec);
+        generoSelec == "m" || generoSelec == "M" ? console.log("masculino") : console.log("feminino"); 
+    })
+}
+
+function pegaClasse() {
+    classeInput.forEach(elemento => elemento.addEventListener('click', () => {
+        classeSelec = document.querySelector("[data-classe]").value;
+        console.log(classeSelec);
+    }))
+}
+
+pegaNome();
+pegaGenero();
+pegaClasse();
